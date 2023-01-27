26dd4d6b54d3c1e31c28107f227936fcd032ac3d
saeed <saeed@blockville.co>
2023-01-27 11:36:05 +0300

    Init : add base files

---
+++ Runtime/EventSystem/Interface/IEventService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SashUnity.EventSystem.Interface
+{
+    public interface IEventService
+    {
+        void Subscribe<T>(int eventId, Action<T> eventClass);
+        void RemoveEvent<T>(int eventId, Action<T> eventClass);
+        void Fire<T>(int eventId, T eventClass);
+    }
+}
---
+++ Runtime/EventSystem/Service/EventService.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using SashUnity.EventSystem.Interface;
+using UnityEngine;
+
+namespace SashUnity.EventSystem.Service
+{
+    /// <summary>
+    /// for better usage it's better to define a static class with int const variables for handling ids or create a enum for that
+    /// Each event need a specific class or struct for adding any type of data to event 
+    /// </summary>
+    public class EventService : IEventService
+    {
+        private readonly Dictionary<int, List<Delegate>> _events = new();
+        private Delegate _lastFunc;
+
+        public void Subscribe<T>(int eventId, Action<T> eventClass)
+        {
+            if (!_events.ContainsKey(eventId))
+            {
+                _events.Add(eventId, new List<Delegate>());
+            }
+
+            _events[eventId].Add(eventClass);
+        }
+
+        public void RemoveEvent<T>(int eventId, Action<T> eventClass)
+        {
+            if (_events.ContainsKey(eventId))
+            {
+                _events[eventId].Remove(eventClass);
+            }
+        }
+
+        public void Fire<T>(int eventId, T eventClass)
+        {
+            if (_events.ContainsKey(eventId))
+            {
+                var events = _events[eventId];
+                foreach (var func in events)
+                {
+                    _lastFunc = func;
+                    try
+                    {
+                        func.DynamicInvoke(eventClass);
+                    }
+                    catch (Exception e)
+                    {
+                        Debug.Log($"Bug in EventId:{eventId} Event : {e.Message} \n " +
+                                  $"{_lastFunc.Method.Name} \n " +
+                                  $"{_lastFunc.Method.Module} \n " +
+                                  $"{_lastFunc.GetType()} \n " +
+                                  $"{_lastFunc.Target}");
+                    }
+                }
+            }
+        }
+    }
+}
